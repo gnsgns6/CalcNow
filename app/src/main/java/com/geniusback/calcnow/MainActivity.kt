81be0c537ed570b206feda62fa130be407da1625
GNS VS <gnsgns6@outlook.com>
2024-01-14 18:16:41 +0530

    Amend Buttons

--- app/src/main/java/com/geniusback/calcnow/MainActivity.kt
+++ app/src/main/java/com/geniusback/calcnow/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.geniusback.calcnow
 
+import android.icu.text.ListFormatter.Width
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.geniusback.calcnow.ui.theme.CalcNowTheme
 
 class MainActivity : ComponentActivity()
@@ -23,11 +44,14 @@ class MainActivity : ComponentActivity()
             CalcNowTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(state = rememberScrollState())
                 ) {
-                    val a = Calc("GNSGNS");
-                    CalcView(a.calcName("GNEISS"))
+                    ScreenView()
+                    KeyView()
+                    val a = Calc("LEGACY")
+//                    CalcView(a.calcName("GNEISS"))
                 }
 
             }
@@ -43,11 +67,51 @@ fun CalcView(name2:String){
         text="Hello $name2"
     )
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CalcNowTheme {
-        CalcView("Android")
+fun ScreenView(){
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .background(color = Color.Blue)
+            .size(height = 180.dp, width = 220.dp),
+        elevation= CardDefaults.cardElevation(defaultElevation = 12.dp))
+    { Text(text = "ScreenView") }
+}
+@Composable
+fun KeyView(){
+    Card(
+        modifier = Modifier
+            .padding(12.dp, top = 220.dp)
+            .background(color = Color.Green)
+            .size(height = 520.dp, width = 220.dp),
+        elevation= CardDefaults.cardElevation(defaultElevation = 12.dp))
+    {
+        Row {
+            Button(onClick = { /*TODO*/ } ,modifier=Modifier
+                    .padding(12.dp), shape = RoundedCornerShape(10.dp))
+            {
+                Text(text = "7")
+            }
+
+        }
     }
 }
+//@Preview(showSystemUi = true, showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    CalcNowTheme {
+//        CalcView("Android")
+//    }
+//}
+//@Composable
+//fun ScreenViewPreview() {
+//    CalcNowTheme {
+//        ScreenView()
+//    }
+//}
+//@Composable
+//fun KeyViewPreview(){
+//    CalcNowTheme {
+//        KeyView()
+//    }
+//}
